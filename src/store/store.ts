e4cb66a261acfb5a7d94e26ae39dd54e45a899a4
nikvoi <tivkaipivka@gmail.com>
2024-03-28 16:22:03 +0300

    delete: store function init

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,37 +1,10 @@
-import { Reducer, configureStore } from '@reduxjs/toolkit'
-import authReducer, { IAuth } from './slices/authSlice'
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './slices/authSlice'
 
 export type RootState = ReturnType<typeof store.getState>
 
-const getTokenFromCookie = (): string | null => {
-	return (
-		document.cookie.replace(
-			/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
-			'$1'
-		) || null
-	)
-}
-
-const getUserEmailFromLocalStorage = (): string | null => {
-	return localStorage.getItem('userEmail') || null
-}
-
-const getUserPhotoURLFromLocalStorage = (): string | null => {
-	return localStorage.getItem('photoURL') || null
-}
-
-const initialAuthState: IAuth = {
-	isAuthenticated: !!getTokenFromCookie(),
-	token: getTokenFromCookie(),
-	userEmail: getUserEmailFromLocalStorage(),
-	photoURL: getUserPhotoURLFromLocalStorage(),
-}
-
 export const store = configureStore({
 	reducer: {
-		auth: authReducer as Reducer<IAuth>,
-	},
-	preloadedState: {
-		auth: initialAuthState,
+		auth: authReducer,
 	},
 })
